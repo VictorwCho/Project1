1afbb7703bf36b95bc9412a199924fc22e54a054
victorcho@outlook.com <BananaBread09!>
2022-12-26 20:52:13 -0800

    Hide database credentials.

--- server/app.js
+++ server/app.js
@@ -1,12 +1,13 @@
+require('dotenv').config()
 const express = require('express');
 const mysql = require('mysql2');
 
 // create the connection to database
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'employees'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_EMPLOYEES
 });
 
 
